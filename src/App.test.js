7e91e163d8317d6aacec290ad5331fb554f0f084
Sean Tran <141064686+Guitar-Fan@users.noreply.github.com>
2025-10-04 15:59:28 +0000

    Tired Chess

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders chess game', () => {
-  render(<App />);
-  const heading = screen.getByText(/Chess Game/i);
-  expect(heading).toBeInTheDocument();
-});
